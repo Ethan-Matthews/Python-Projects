36daf3a07986ed6ad677175b82bc6f84a2b4a0fa
Ethan Matthews <w0420789@nscc.ca>
2018-10-01 08:32:39 -0300

    Ex04 comments

--- Exercises/Ex4-Luggage Weights Program.py
+++ Exercises/Ex4-Luggage Weights Program.py
@@ -27,19 +27,19 @@ Description:     Evaluate whether or not a surcharge will be applied to the user
 def main(): #<-- Don't change this line!
     #Write your code below. It must be indented!
 
-    surcharge = 25.00
-    weightLimit = 50
+    surcharge = 25.00       #surcharge in dollars.
+    weightLimit = 50        #weight limit 50lbs.
 
     print("Welcome to the Surcharge Application!\n")
-    weightLbs = float(input("Please enter the weight of your luggage in pounds: "))
+    weightLbs = float(input("Please enter the weight of your luggage in pounds: "))     #User input luggage weight.
 
-    if weightLbs > weightLimit:
-        print("\nYou will be charged ${0:.2f}. Your luggage is over the {1}lbs. weight limit.".format(surcharge, weightLimit))
+    if weightLbs > weightLimit:     #conditional statment - greater than.
+        print("\nYou will be charged ${0:.2f}. Your luggage is over the {1}lbs. weight limit.".format(surcharge, weightLimit))      #print if greater than, surcharge and weight limit.
 
-    if weightLbs <= weightLimit:
-        print("\nYou will not be charged an extra ${0:.2f}. Your luggage is not over the {1}lbs. weight limit.".format(surcharge, weightLimit))
+    if weightLbs <= weightLimit:        #conditional statment - less than or equal too.
+        print("\nYou will not be charged an extra ${0:.2f}. Your luggage is not over the {1}lbs. weight limit.".format(surcharge, weightLimit))     #print if les than or equal too.
     
-    print("\nThankyou, have a nice trip!")
+    print("\nThankyou, have a nice trip!")      #print goodbye/thankyou banner.
 
     #Your code ends on the line above
 
