28b71b7dfa7aab2385a5264dff1c53dbb96b874c
Ethan Matthews <w0420789@nscc.ca>
2018-10-05 10:41:42 -0300

    changes in comments

--- Exercises/WageCalculator1.1.py
+++ Exercises/WageCalculator1.1.py
@@ -1,7 +1,7 @@
 """
 Prompt user to input hours worked.  Varaibles used are hoursWorked, hourlyRate.
 calculate pay from number of hours. Variables used are regPay, overTime.
-conditional statments if hours are over 40, pay increrases to 1.5x.
+conditional IF statement, if hours are over 40 pay increrases to 1.5x.
 output variables in .format string.
 """
 
@@ -15,19 +15,22 @@ Description:     Calculate pay and overtime after 40hrs.
 
 def main(): #<-- Don't change this line!
     #Write your code below. It must be indented!
+
     hourlyRate = 10.00
     overtimeBracket = 40
+    
     print("Welcome to the Pay Calculator!\n")
     hoursWorked = float(input("Enter the number of hours worked: "))
 
     regPay = hoursWorked * hourlyRate                                                                       #Calc for regular pay.
-    overTime = ((hoursWorked - overtimeBracket) * (hourlyRate * 1.5)) + (overtimeBracket * hourlyRate)      #clac fro overtime.
+    overTime = ((hoursWorked - overtimeBracket) * (hourlyRate * 1.5)) + (overtimeBracket * hourlyRate)      #Clac for overtime.
 
-    if hoursWorked > overtimeBracket:                                                                       #conditional statment for overtime bracket.
-        print("\nYour total pay with overtime is ${0:.2f}.".format(overTime))                               #print IF pay has overtime.
+    if hoursWorked > overtimeBracket:                                                                       #Conditional statement for overtimeBracket.
+        print("\nYour total pay with overtime is ${0:.2f}.".format(overTime))                               #Print IF pay has overtime.
     else:
-        print("\nYou total pay is ${0:.2f}.".format(regPay))                                                #print IF pay doesn't have overtime.
+        print("\nYou total pay is ${0:.2f}.".format(regPay))                                                #Print IF pay doesn't have overtime.
 
+    print("\nThankyou for using the Pay Calculator!")
 
     #Your code ends on the line above
 
